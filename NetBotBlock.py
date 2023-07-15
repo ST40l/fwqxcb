7250ef610fcbff8b7db10e2fa9f248ea1760a419
Sy3roX <137301764+netoverr@users.noreply.github.com>
2023-07-15 20:15:35 +0300

    Create NetBotBlock.py

---
+++ NetBotBlock.py
@@ -0,0 +1,50 @@
+import logging
+
+logging.basicConfig(filename='uygulama.log', level=logging.INFO)
+
+def main():
+    proxies = load_proxies_from_file("C:\\Users\\Xyeta\\Desktop\\proxy (2).txt")
+    bot_region = "90"  # Bölgenizin kodunu buraya girin
+    target_region = "TR"  # Hedef bölgenin kodunu buraya girin (Örneğin, "FR" Fransa)
+
+    while True:
+        try:
+            ip = get_public_ip()
+            if not is_region_allowed(ip, bot_region, target_region):
+                block_ip(ip)
+            else:
+                logging.info("İstek başarıyla tamamlandı")
+                print("İstek başarıyla tamamlandı.")
+        except Exception as e:
+            logging.error("Bir hata oluştu: " + str(e))
+            print("Bir hata oluştu:", str(e))
+
+def load_proxies_from_file(filename):
+    with open(filename, "r") as file:
+        proxies = [proxy.strip() for proxy in file]
+    return proxies
+
+def select_fastest_proxy(proxies):
+    # Proxylerin performansını değerlendirerek en hızlı olanı seçin
+    # Bu kısmı gereksinimlerinize ve kullanacağınız yöntemlere göre uyarlayın
+    fastest_proxy = proxies[0]  # Örnek olarak en hızlı proxyyi seçelim
+    return fastest_proxy
+
+def get_public_ip():
+    # Halka açık IP adresini almak için uygun bir yöntem kullanın
+    pass
+
+def is_region_allowed(ip, bot_region, target_region):
+    # IP adresinin belirli bir bölgeden olup olmadığını kontrol etmek için bir API kullanabilirsiniz
+    # Örneğin, ipstack.com gibi bir IP konumlandırma servisini kullanabilirsiniz
+    # Bu API'yi kullanarak IP'nin bölge bilgisini alabilir ve bot_region ile karşılaştırabilirsiniz
+    # Ayrıca hedef bölgeyle de karşılaştırma yaparak kontrol edebilirsiniz
+    pass
+
+def block_ip(ip):
+    # Belirli bir IP adresini devre dışı bırakmak için gereken kodu buraya yazın
+    # Bu fonksiyon, ilgili IP adresini devre dışı bırakacak işlemleri gerçekleştirmelidir
+    print("IP adresi devre dışı bırakıldı:", ip)
+
+if __name__ == "__main__":
+    main()
